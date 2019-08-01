02335f9c912645d32a494e82230092ac94fa797d
Lucas Geurtjens <Lucas.geurtjens@griffithuni.edu.au>
2019-08-01 14:23:27 +1000

    Added image to Account page

--- account.js
+++ account.js
@@ -1,7 +1,7 @@
 module.exports = function(app,path){
 
-    app.get('/accounts',function(req,res){
-    let filepath = path.resolve('./www/accounts.html');
+    app.get('/account',function(req,res){
+    let filepath = path.resolve('./www/account.html');
     res.sendFile(filepath);
 
     });
